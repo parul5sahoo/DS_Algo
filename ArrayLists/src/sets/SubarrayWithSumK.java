0d3d18c226e9686be38efaffa50dd5589cdab435
Parul Sahoo <parulsahoo5jan@gmail.com>
2020-04-20 22:19:04 +0530

    sets subarray Qs

---
+++ ArrayLists/src/sets/SubarrayWithSumK.java
@@ -0,0 +1,38 @@
+
+package sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubarrayWithSumK {
+    public static void main(String[] args){
+        
+        int[] a= {2,1,3,-4,-2};
+        int k = 9;
+        boolean found = false;
+        /*for(int i=0; i<a.length; i++){
+            int sum =0;
+            for(int j=i; j<a.length; j++){
+                sum += a[j];
+                if(sum == 0){
+                    found = true;
+                    break;
+                }
+            }
+            if(found) break;
+        }*/
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+        for(int element : a) {
+            set.add(sum);
+            sum += element;
+            if(set.contains(sum-k)){
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("found " + found);    
+    }
+}
+
---
+++ ArrayLists/src/sets/SubarrayWithZeroSum.java
@@ -0,0 +1,33 @@
+package sets;
+import java.util.*;
+public class SubarrayWithZeroSum {
+    
+    public static void main(String[] args){
+        
+        int[] a= {2,1,3,-4,-2};
+        boolean found = false;
+        /*for(int i=0; i<a.length; i++){
+            int sum =0;
+            for(int j=i; j<a.length; j++){
+                sum += a[j];
+                if(sum == 0){
+                    found = true;
+                    break;
+                }
+            }
+            if(found) break;
+        }*/
+        Set<Integer> set = new HashSet<>();
+        int sum = 0;
+        for(int element : a) {
+            set.add(sum);
+            sum += element;
+            if(set.contains(sum)){
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("found " + found);    
+    }
+}
